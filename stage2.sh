e2e2ca40750274bcd71777e13317df67048ba3ab
SilverAngel <koehler.oliver@gmail.com>
2024-08-06 10:56:33 +0200

    stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -26,15 +26,9 @@ cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes
 $gcs https://github.com/ltdrdata/ComfyUI-Manager.git
 
 # Workspace
-$gcs https://github.com/AIGODLIKE/AIGODLIKE-ComfyUI-Translation.git
-$gcs https://github.com/crystian/ComfyUI-Crystools.git
-$gcs https://github.com/crystian/ComfyUI-Crystools-save.git
 
 # General
-$gcs https://github.com/bash-j/mikey_nodes.git
-$gcs https://github.com/chrisgoringe/cg-use-everywhere.git
 $gcs https://github.com/cubiq/ComfyUI_essentials.git
-$gcs https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes.git
 $gcs https://github.com/jags111/efficiency-nodes-comfyui.git
 $gcs https://github.com/kijai/ComfyUI-KJNodes.git
 $gcs https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
@@ -46,7 +40,6 @@ $gcs https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git
 $gcs https://github.com/cubiq/ComfyUI_InstantID.git
 $gcs https://github.com/cubiq/ComfyUI_IPAdapter_plus.git
 $gcs https://github.com/Fannovel16/comfyui_controlnet_aux.git
-$gcs https://github.com/florestefano1975/comfyui-portrait-master.git
 $gcs https://github.com/Gourieff/comfyui-reactor-node.git
 $gcs https://github.com/huchenlei/ComfyUI-layerdiffuse.git
 $gcs https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git
@@ -56,20 +49,9 @@ $gcs https://github.com/mcmonkeyprojects/sd-dynamic-thresholding.git
 $gcs https://github.com/storyicon/comfyui_segment_anything.git
 $gcs https://github.com/twri/sdxl_prompt_styler.git
 
-# Video
-$gcs https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git
-$gcs https://github.com/FizzleDorf/ComfyUI_FizzNodes.git
-$gcs https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved.git
-$gcs https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
-$gcs https://github.com/melMass/comfy_mtb.git
-$gcs https://github.com/MrForExample/ComfyUI-AnimateAnyone-Evolved.git
-
 # More
 $gcs https://github.com/cubiq/ComfyUI_FaceAnalysis.git
 $gcs https://github.com/MrForExample/ComfyUI-3D-Pack.git
-$gcs https://github.com/pythongosssss/ComfyUI-WD14-Tagger.git
-$gcs https://github.com/SLAPaper/ComfyUI-Image-Selector.git
-$gcs https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git
 $gcs https://github.com/WASasquatch/was-node-suite-comfyui.git
 
 
